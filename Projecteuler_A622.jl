4cc543b924f3a1261b3d4fa52cb3f45b9c75c523
Da-niel-01 <daniel20010806@gmail.com>
2025-08-05 13:17:52 +0200

    "check"

--- Projecteuler_A622.jl
+++ Projecteuler_A622.jl
@@ -1,7 +1,8 @@
 function ProjectEuler_A622(s)
     #korrekt aber schlechte Laufzeit
-    sum = 0
-    for x= 2:2^s
+    sum = 2^s
+    L=[2^s]
+    for x= 2:2^(s-1)
         Add = false
         if 2^s % (x-1) == 1
             Add = true
@@ -13,33 +14,12 @@ function ProjectEuler_A622(s)
             end
             if Add
                 sum += x
+                L=push!(L, x)
             end
         end
     end
-    return sum
+    return sum,L
 end
 
-#ProjectEuler_A622(8)
+ProjectEuler_A622(16)
 
-
-function test(s)
-    L=[]
-    for x= 2:2^s
-        Add = false
-        if 2^s % (x-1) == 1
-            Add = true
-            for n=1:s-1
-                if 2^n % (x-1) == 1
-                    Add = false
-                    break
-                end
-            end
-            if Add
-                L = push!(L, x)
-            end
-        end
-    end
-    return L
-end
-
-test(8)
