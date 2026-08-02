07e99844d08a7bd76f57821d78cf081c85673cb2
Da-niel-01 <daniel20010806@gmail.com>
2026-08-02 17:37:00 +0200

    working

--- Projecteuler_A116.py
+++ Projecteuler_A116.py
@@ -1,1 +1,23 @@
-print("hello world")
+import math
+
+
+def bin(n,k):
+    return math.factorial(n)/(math.factorial(k)*math.factorial(n-k))
+
+def f(n,j):
+    possib = 0
+    t = 1
+    while  t*j <= n:
+        possib += bin(n-t*(j-1),t)
+        t+= 1
+        #print(possib)
+    return possib
+       
+def sol(n):
+    total = 0
+    for j in [2,3,4]:
+        total += f(n,j)
+    return total
+
+#print(f(5,2))
+print(sol(50))
